a2fa010472a890e9196ecdae22a8e03458176adf
Eurekahah <a984036428@gmail.com>
2024-08-29 15:50:31 +0800

    修改小bug，补充注释

--- user/sleep.c
+++ user/sleep.c
@@ -2,10 +2,10 @@
 #include "user/user.h"
 
 int main(int argc, char const *argv[]) {
-    if (argc != 2) { //
+    if (argc != 2) { //判断参数数量
         printf("error! usage: sleep <time>\n");
         exit(1);
     }
-    sleep(atoi(argv[1]));
+    sleep(atoi(argv[1]));//系统调用sleep函数
     exit(0);
 }
